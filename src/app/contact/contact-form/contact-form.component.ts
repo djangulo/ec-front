65ccc782636022f9cb67a96d3ced6d1b528e0de6
Denis Angulo <DenisJose.AnguloLuna@alorica.com>
2017-05-16 17:54:49 -0400

    initial styling for contact form

--- src/app/contact/contact-form/contact-form.component.ts
+++ src/app/contact/contact-form/contact-form.component.ts
@@ -34,17 +34,6 @@ export class ContactFormComponent implements OnInit {
     'subject': '',
     'message': ''
   }
-  validationMessages = {
-    'name': {
-      'required':      'Name is required.',
-      'minlength':     'Name must be at least 4 characters long.',
-      'maxlength':     'Name cannot be more than 24 characters long.',
-      'forbiddenName': 'Someone named "Bob" cannot be a hero.'
-    },
-    'power': {
-      'required': 'Power is required.'
-    }
-  };
 
 
   ngOnInit() {
