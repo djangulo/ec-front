a1af5ed53aec6d599d08325ef22462a5e5bd638e
Denis Angulo <denis.angulo@linekode.com>
2017-06-04 21:15:07 -0400

    some aesthetic changes to works
    Signed-off-by: Denis Angulo <denis.angulo@linekode.com>

--- src/app/contact/contact-animations.ts
+++ src/app/contact/contact-animations.ts
@@ -8,10 +8,10 @@ import {
 
 export const Animations = {
     flySecondIn: trigger('flySecondIn', [
-        state('in', style({ transform: 'translate3d(0, 0, 0)', opacity: 1})),
-        state('noSelection', style({ transform: 'translate3d(54%, 0, 0)', opacity: 1})),
+        state('in', style({ transform: 'translate3d(34%, 0, 0)', opacity: 1})),
+        state('noSelection', style({ transform: 'translate3d(34%, 0, 0)', opacity: 1})),
         transition('void => *', [
-            style({transform: 'translate3d(0, 200%, 0)', opacity: 0}),
+            style({transform: 'translate3d(34%, 200%, 0)', opacity: 0}),
             animate('500ms ease-out')
         ]),
     ]),
